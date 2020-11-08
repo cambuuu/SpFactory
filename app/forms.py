a35fd3f85fa2e8f06b6a42864ca0499319659789
cambuuu <ricardo.sepulveda86@gmail.com>
2020-11-08 16:39:00 -0300

    con parte del CRUD

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,21 @@
 from django import forms
-from .models import Contacto
+from .models import Contacto, Producto
 
 class ContactoForm(forms.ModelForm):
 
     class Meta:
         model = Contacto
-        #fields = ["nombre","correo","motivo_consulta","mensaje"]
         fields = '__all__'
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class ProductoForm(forms.ModelForm):
+
+    class Meta:
+        model = Producto
+        fields = '__all__'
+        widgets = {
+            'fecha_producto': DateInput()
+
+        }
